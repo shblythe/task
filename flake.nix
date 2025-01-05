00cd19c317d2cb2769a945929d3196300e6b724a
Stephen Blythe <stephen@urwick.co.uk>
2025-01-05 09:51:33 +0000

    Added flake
    Run `nix develop` to get an environment to build and run this in.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+    outputs = {self, nixpkgs}:
+        let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in {
+            devShells.${system}.default = pkgs.mkShell {
+                name = "task-dev";
+                buildInputs = with pkgs; [
+                    cargo
+                    clippy
+                    rustc
+                ];
+                shellHook = ''
+                    # bash to run when you enter the shell
+                    ls
+                '';
+            };
+        };
+}
